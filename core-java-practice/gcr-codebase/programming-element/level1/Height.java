32919260e214aace960634955642d4b76a25f461
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 09:56:03 +0530

    [khushboo] add: Height,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level1/Height.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Height{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int height= sc.nextInt();
+		 double inch = height*2.54;
+		 double feet= inch*12;
+		System.out.println("in cm "+ inch +"in feet"+feet);
+		}}
