55a64b47d2b77e7e50eb4e26ce90cd3df23cb989
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 09:57:24 +0530

    [khushboo] add: Km_miles,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level1/Km_miles.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class  Km_miles{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int km= sc.nextInt();
+		System.out.println(km*1.6+"miles");
+		}}
