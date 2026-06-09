77bbf3a2c38741f3c372b219ba9ec8870e8d1ef7
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 10:46:03 +0530

    [khushboo] add:  Distance,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level2/Distance.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int  dis = sc.nextInt();
+		  double yard = dis*3;
+		  double mile= yard*1760;
+		System.out.println(yard+" "+mile);
+		}}
