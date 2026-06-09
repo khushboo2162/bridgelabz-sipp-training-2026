ac5b606f800b057ce5246a2a7dd57971ed989b65
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 09:56:54 +0530

    [khushboo] add: Km,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level1/Km.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class  Km {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		double km= 10.8; 
+		System.out.println(km*1.6);
+		}}
+		
