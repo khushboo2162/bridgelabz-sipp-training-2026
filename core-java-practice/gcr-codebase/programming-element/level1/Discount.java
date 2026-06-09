a4c66d53eddbf52a0c21a59388e10b082c8411f4
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 09:53:14 +0530

    [khushboo] add: Discount,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level1/Discount.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class  Discount{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int fee= 125000;
+		int dis= 10;
+		int per=(125000*10)/100;
+		int sub= fee- per;
+		System.out.println(sub);
+		
+		}}
