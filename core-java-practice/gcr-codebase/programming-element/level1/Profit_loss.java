3d3735840f9a2773a2f2d4e03ed996cf5e4fba6c
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 09:59:44 +0530

    [khushboo] add: profit_loss,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level1/Profit_loss.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Profit_loss{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		 int cp=129;
+		 int sp=191;
+		 int profit=sp-cp;
+		 int per=(profit*100)/cp;
+		 System.out.println(profit+"\n"+per);}}
