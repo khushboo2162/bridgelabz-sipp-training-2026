8c985365b4ec5678cefa3c7ec1b6181151bd5f4c
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 09:52:45 +0530

    [khushboo] add: Avg,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level1/Avg.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Avg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int che=96;
+		int phy=95;
+		int math=67;
+		int avg= (che+phy+math)/3;
+		System.out.println(avg);
+}}
