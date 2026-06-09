4410d0d7c7e4e771810ca0f603bb77bb7d9beb97
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 10:42:53 +0530

    [khushboo] add:  Area,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level2/Parameter.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Parameter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int side = sc.nextInt();
+		int p= side*side*side*side;
+		System.out.println(p);
+		}}
