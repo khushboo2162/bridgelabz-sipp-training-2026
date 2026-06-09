6cf7b8ed087ce9e545f8a352206584e26b60c722
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-09 10:00:18 +0530

    [khushboo] add: Volume,java file

---
+++ core-java-practice/gcr-codebase/programming-element/level1/Volume.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class  Volume{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int r=6378;
+		 double volume = (4/3)*3.14*r*r*r;
+		System.out.println( volume);
+		}}
+		
