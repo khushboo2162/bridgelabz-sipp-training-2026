dedb897e66f3c7d3088545b3b2b484fefd269034
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-06-23 10:11:12 +0530

    [khushboo]add: add book constructor file

---
+++ oops-practice/gcr-codebase/fundamental_programming_element/level1/BookConstructor.java
@@ -0,0 +1,22 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    public Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b2 = new Book("Java", "James Gosling", 599);
+    }
+}
